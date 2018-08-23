950a89d8116a1c0762ce912a9afafc8ce29411cd
shengqi2 <1260961114@qq.com>
2018-08-23 19:41:27 +0800

    first

---
+++ src/main/java/com/cl/concurrence/ConcurrenceApplication.java
@@ -0,0 +1,12 @@
+package com.cl.concurrence;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConcurrenceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConcurrenceApplication.class, args);
+	}
+}
